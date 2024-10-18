0db778da62e339ab1c3c2f66ea020a31102c47e3
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2024-10-18 08:15:57 -0400

    1.5.0.1 (#3)
    I can't believe one single additional check in some obscure file necessitated this.

---
+++ Source/ScrapAnything/HarmonyPatches/HarmonyPatch_GenRecipe_MakeRecipeProducts.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace Cerespirin.ScrapAnything.HarmonyPatches
+{
+	[HarmonyPatch(typeof(GenRecipe), nameof(GenRecipe.MakeRecipeProducts))]
+	public static class HarmonyPatch_GenRecipe_MakeRecipeProducts
+	{
+		public static IEnumerable<Thing> Postfix(IEnumerable<Thing> __result, RecipeDef recipeDef, Pawn worker, List<Thing> ingredients, Precept_ThingStyle precept = null, ThingStyleDef style = null, int? overrideGraphicIndex = null)
+		{
+			foreach (Thing thing in __result)
+			{
+				yield return thing;
+			}
+
+			if (recipeDef.HasModExtension<ScrapAnythingExt>())
+			{
+				MethodInfo postProcessProduct = typeof(GenRecipe).GetMethod("PostProcessProduct", BindingFlags.NonPublic | BindingFlags.Static);
+				{
+					foreach (Thing thing3 in ingredients)
+					{
+						foreach (Thing product in thing3.SmeltProducts_Old())
+						{
+							//yield return GenRecipe.PostProcessProduct(product, recipeDef, worker, precept, style, overrideGraphicIndex);
+							yield return (Thing)postProcessProduct.Invoke(null, new object[] { product, recipeDef, worker, precept, style, overrideGraphicIndex });
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Source/ScrapAnything/MyRecipeHelper.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace Cerespirin.ScrapAnything.HarmonyPatches
+{
+	internal static class MyRecipeHelper
+	{
+		public static IEnumerable<Thing> SmeltProducts_Old(this Thing thisThing)
+		{
+			// RimWorld version 1.5 added an additional check which totally destroyed the mod's functionality.
+			// This check was probably added for a reason, so instead bring back the old version for our use.
+			List<ThingDefCountClass> costListAdj = thisThing.def.CostListAdjusted(thisThing.Stuff, true);
+			int num2;
+			for (int i = 0; i < costListAdj.Count; i = num2 + 1)
+			{
+				if (!costListAdj[i].thingDef.intricate)// && costListAdj[i].thingDef.smeltable)
+				{
+					int num = GenMath.RoundRandom((float)costListAdj[i].count * 0.25f);
+					if (num > 0)
+					{
+						Thing thing = ThingMaker.MakeThing(costListAdj[i].thingDef, null);
+						thing.stackCount = num;
+						yield return thing;
+					}
+				}
+				num2 = i;
+			}
+			if (thisThing.def.smeltProducts != null)
+			{
+				for (int i = 0; i < thisThing.def.smeltProducts.Count; i = num2 + 1)
+				{
+					ThingDefCountClass thingDefCountClass = thisThing.def.smeltProducts[i];
+					Thing thing2 = ThingMaker.MakeThing(thingDefCountClass.thingDef, null);
+					thing2.stackCount = thingDefCountClass.count;
+					yield return thing2;
+					num2 = i;
+				}
+			}
+			yield break;
+		}
+	}
+}
