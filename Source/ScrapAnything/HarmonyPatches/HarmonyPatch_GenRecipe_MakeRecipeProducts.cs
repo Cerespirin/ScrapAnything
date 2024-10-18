5327ee8d334d0ca4613923fb7e277d1c1d5bcc2c
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2024-10-18 07:29:22 -0400

    Hopefully this'll work!
    Seriously, now we're bringing reflection into this? I can't believe one single additional check in some obscure file necessitated this bullshit.

--- Source/ScrapAnything/HarmonyPatches/HarmonyPatch_GenRecipe_MakeRecipeProducts.cs
+++ Source/ScrapAnything/HarmonyPatches/HarmonyPatch_GenRecipe_MakeRecipeProducts.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using HarmonyLib;
@@ -12,7 +13,7 @@ namespace Cerespirin.ScrapAnything.HarmonyPatches
 	[HarmonyPatch(typeof(GenRecipe), nameof(GenRecipe.MakeRecipeProducts))]
 	public static class HarmonyPatch_GenRecipe_MakeRecipeProducts
 	{
-		public static IEnumerable<Thing> Postfix(IEnumerable<Thing> __result, RecipeDef recipeDef, Pawn worker, List<Thing> ingredients, Thing dominantIngredient, IBillGiver billGiver, Precept_ThingStyle precept = null, ThingStyleDef style = null, int? overrideGraphicIndex = null)
+		public static IEnumerable<Thing> Postfix(IEnumerable<Thing> __result, RecipeDef recipeDef, Pawn worker, List<Thing> ingredients, Precept_ThingStyle precept = null, ThingStyleDef style = null, int? overrideGraphicIndex = null)
 		{
 			foreach (Thing thing in __result)
 			{
@@ -21,12 +22,16 @@ namespace Cerespirin.ScrapAnything.HarmonyPatches
 
 			if (recipeDef.HasModExtension<ScrapAnythingExt>())
 			{
-				int num;
-				for (int i = 0; i < recipeDef.specialProducts.Count; i = num + 1)
+				MethodInfo postProcessProduct = typeof(GenRecipe).GetMethod("PostProcessProduct", BindingFlags.NonPublic | BindingFlags.Static);
+
+				//int num;
+				//for (int i = 0; i < recipeDef.specialProducts.Count; i = num + 1)
+				foreach (SpecialProductType specialProductType in recipeDef.specialProducts)
 				{
-					for (int j = 0; j < ingredients.Count; j = num + 1)
+					//for (int j = 0; j < ingredients.Count; j = num + 1)
+					foreach (Thing thing3 in ingredients)
 					{
-						Thing thing3 = ingredients[j];
+						//Thing thing3 = ingredients[j];
 						//SpecialProductType specialProductType = recipeDef.specialProducts[i];
 						//if (specialProductType != SpecialProductType.Butchery)
 						{
@@ -34,13 +39,14 @@ namespace Cerespirin.ScrapAnything.HarmonyPatches
 							{
 								foreach (Thing product in thing3.SmeltProducts_Old())
 								{
-									yield return GenRecipe.PostProcessProduct(product, recipeDef, worker, precept, style, overrideGraphicIndex);
+									//yield return GenRecipe.PostProcessProduct(product, recipeDef, worker, precept, style, overrideGraphicIndex);
+									yield return (Thing)postProcessProduct.Invoke(null, new object[] { product, recipeDef, worker, precept, style, overrideGraphicIndex });
 								}
 							}
 						}
-						num = j;
+						//num = j;
 					}
-					num = i;
+					//num = i;
 				}
 			}
 		}
